6b433aa595f483ee4ccf4e42a57b9094e1780dda
unknown <maxdbehrens@gmail.com>
2019-11-10 22:17:53 +0000

    Updates

--- src/elements/Accent.js
+++
@@ -1,8 +0,0 @@
-import styled from "styled-components"
-
-const Accent = styled.span`
-color: ${props => props.theme.secondaryText};
-font-style: normal;
-`;
-
-export default Accent;
---
+++ styles-9384b1d7c0a2f2f7d1eb.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[6],{RDVA:function(n,w,o){}}]);
+//# sourceMappingURL=styles-9384b1d7c0a2f2f7d1eb.js.map
